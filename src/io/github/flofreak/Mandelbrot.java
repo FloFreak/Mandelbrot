6a95901d409a1bbb0eace2017c46fe5b39e10f9c
FloFreak <minecraftfreak1210@gmail.com>
2018-07-02 11:51:46 +0200

    Color

--- src/io/github/flofreak/Mandelbrot.java
+++ src/io/github/flofreak/Mandelbrot.java
@@ -1,16 +1,21 @@
 package io.github.flofreak;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 class Mandelbrot {
     static final int WIDTH = 800;
     static final int HEIGHT = 800;
     private static final int MAX = 1000;
+    private static int[] colors = new int[MAX];
 
-    //public static BufferedImage calculate(double pos_x, double pos_y, double zoom) {
+    //static BufferedImage calculate(double pos_x, double pos_y, double zoom) {
     static BufferedImage calculate() {
+
+        for (int i = 0; i < MAX; i++)
+            colors[i] = Color.HSBtoRGB(i / 256f, 1, i / (i + 8f));
+
         BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-        int black = 0x000000, white = 0xFFFFFF;
 
         for (int row = 0; row < HEIGHT; row++) {
             for (int col = 0; col < WIDTH; col++) {
@@ -24,8 +29,8 @@ class Mandelbrot {
                     x = x_new;
                     iterations++;
                 }
-                if (iterations < MAX) image.setRGB(col, row, white);
-                else image.setRGB(col, row, black);
+                if (iterations < MAX) image.setRGB(col, row, colors[iterations]);
+                else image.setRGB(col, row, 0x000000);
             }
         }
         return image;
