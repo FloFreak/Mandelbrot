f213446ab182fc81b1b62f5073aa5d7c1d8ca0b5
FloFreak <minecraftfreak1210@gmail.com>
2018-07-04 09:44:48 +0200

    Added Julia, to check expandability

---
+++ src/io/github/flofreak/algorithms/Julia.java
@@ -0,0 +1,49 @@
+/*
+    https://rosettacode.org/wiki/Julia_set#Java
+ */
+package io.github.flofreak.algorithms;
+
+import io.github.flofreak.GUI;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Julia extends BaseAlgorithm {
+    private final static int MAX = Integer.parseInt(GUI.cfg.getProperty("imgMaxIterations"));
+
+    private final GUI gui;
+
+    public Julia(GUI gui) {
+        this.gui = gui;
+    }
+
+    @Override
+    public BufferedImage calculate() {
+        BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+
+        double cX = -0.7;
+        double cY = 0.27015;
+        double moveX = 0;
+        double moveY = 0;
+        double zx;
+        double zy;
+
+        double zoom = 1;
+
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                zx = 1.5 * (x - WIDTH / 2) / (0.5 * zoom * WIDTH) + moveX;
+                zy = (y - HEIGHT / 2) / (0.5 * zoom * HEIGHT) + moveY;
+                float i = MAX;
+                while (zx * zx + zy * zy < 4 && i > 0) {
+                    double tmp = zx * zx - zy * zy + cX;
+                    zy = 2.0 * zx * zy + cY;
+                    zx = tmp;
+                    i--;
+                }
+                image.setRGB(x, y, Color.HSBtoRGB((MAX / i) % 1, 1, i > 0 ? 1 : 0));
+            }
+        }
+        return image;
+    }
+}
